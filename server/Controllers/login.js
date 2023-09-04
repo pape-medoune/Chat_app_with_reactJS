185a52348ee86868d92aa1a7a8df4617c534b87b
Mouhamedoune  Fall <MEDOUNEFALL126@GMAIL.COM>
2023-09-04 11:52:00 +0000

    Mise à jours

--- server/Controllers/login.js
+++ server/Controllers/login.js
@@ -1,23 +1,54 @@
-const bcrypt = require('bcrypt');
-const Login = (req,res)=>{
-    axios.get("mongodb://localhost:27017/chatapp.users")
-    .then(data => {
-        console.log(data);
-        password = req.body.password;
-        const match = bcrypt.compare(password, data.password);
+// const bcrypt = require('bcrypt');
+// const Login = (req,res)=>{
+//     axios.get("mongodb://localhost:27017/chatapp.users")
+//     .then(data => {
+//         console.log(data);
+//         password = req.body.password;
+//         const match = bcrypt.compare(password, data.password);
         
-        if(match) {
+//         if(match) {
+//             //login
+//             res.send({
+//                 message: "Connection fait avec succés!";
+//             })
+//             res.redirect("/user");
+//         }
+//     })
+//     .catch(err => {
+//         console.log(err); 
+//     })
+
+    
+// }
+
+// module.exports = Login;
+
+const bcrypt = require('bcrypt');
+const MongoClient  =  require("mongodb").MongoClient();
+
+const Login = (req,res)=>{
+   db = client.db("chatapp");
+
+   db.collection("users").find().toArray().then(
+    (err,data)=>{
+        if(err){
+
+            console.log("erreur lors de l'affichage !" );
+            password = req.body.password;
+        const match = bcrypt.compare(password, data.password); 
+        if(match && (req.body.email === data.email)) {
             //login
             res.send({
                 message: "Connection fait avec succés!";
             })
+            
             res.redirect("/user");
         }
-    })
-    .catch(err => {
-        console.log(err); 
-    })
-
+        }else{
+            res.send(data.json());
+        }
+    }
+   )
     
 }
 
--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const register = require("../Controllers/register");
 const Route = express.Router();
+const login  = require("../Controllers/login");
 
 
 //API FOR USERS
 Route.post("/register",register.register);
+Route.post("/login",login.login);
 Route.get("/display",register.displayUser);
 
 module.exports = Route;
